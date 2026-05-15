1111e9f398c7cbbe373dc2b5fa579786ca63ec77
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-05-15 11:56:41 +0200

    Added js logic for the hamburger-menu on desktop and mobile

--- js/utils.js
+++ js/utils.js
@@ -13,6 +13,24 @@ function hideLoader() {
         loader.classList.add("hidden");
     }
 
+
+// Function for hamburger menu toggle
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburgerMenu.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+
+    // Toggle the hamburger icon
+    const icon = hamburgerMenu.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+
+    updateCartCount() // Update cart count when menu is toggled
+}) 
+
+
+// Function to update the cart count in the header
 function updateCartCount() {
     const cart = JSON.parse(localStorage.getItem("cart")) || []
     const count = cart.reduce((acc, item) => acc + item.quantity, 0)
