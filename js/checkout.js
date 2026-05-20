cb5896510bfbfef8d69344537eb983a25e54fec3
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-05-20 11:49:05 +0200

    Added eventListener for cardoption

--- js/checkout.js
+++ js/checkout.js
@@ -1,9 +1,29 @@
 //Loader//
 showLoader()
-window.addEventListener("load", () => {
+window.onLoad = () => {
+    hideLoader()
+}
+
+//When the user clicks on card, cardfields will be shown. When click on vipps, cardfields will be hidden//
+document.addEventListener("DOMContentLoaded", () => {
+    const cardOption = document.querySelector("input[value='card']")
+    const vippsOption = document.querySelector("input[value='vipps']")
+    const cardFields = document.getElementById("card-fields")
+
+    function updateCardFields() {
+        if (cardOption.checked) {
+            cardFields.classList.remove("hidden")
+        } else {
+            cardFields.classList.add("hidden")
+        }
+    }
+
+    cardOption.addEventListener("change", updateCardFields)
+    vippsOption.addEventListener("change", updateCardFields)
+    updateCardFields()
+
     setTimeout(() => {
         hideLoader()
     }, 300)
 })
 
-//When the user clicks
