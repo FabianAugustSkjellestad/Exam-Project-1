100a77843293725e504e779dbb1f7ff9113efcd3
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-05-20 12:22:53 +0200

    Added payment info validation to the checkout-page

--- js/checkout.js
+++ js/checkout.js
@@ -40,3 +40,50 @@ checkoutForm.addEventListener("submit", (event) => {
         window.location.href = "success.html"
     }, 600)
 })
+
+// Payment info validation //
+const cardNumber = document.getElementById("cardNumber")
+const cardNumberError = document.querySelector(".cardNumber-error")
+
+const expiry = document.getElementById("cardExpirydate")
+const expiryError = document.querySelector(".expiry-error")
+
+const cvc = document.getElementById("cardCVC")
+const cvcError = document.querySelector(".cvc-error")
+
+cardNumber.addEventListener("blur", () => {
+    if (!/^\d{16}$/.test(cardNumber.value)) {
+        cardNumberError.textContent = "Card number must be 16 digits."
+        cardNumber.classList.add("input-error")
+        cardNumber.classList.remove("input-success")
+    } else {
+        cardNumberError.textContent = ""
+        cardNumber.classList.remove("input-error")
+        cardNumber.classList.add("input-success")
+    }
+})
+
+expiry.addEventListener("blur", () => {
+    if (!/^\d{2}\/\d{2}$/.test(expiry.value)) {
+        expiryError.textContent = "Expiry date must be in MM/YY format."
+        expiry.classList.add("input-error")
+        expiry.classList.remove("input-success")
+    } else {
+        expiryError.textContent = ""
+        expiry.classList.remove("input-error")
+        expiry.classList.add("input-success")
+    }
+})
+
+cvc.addEventListener("blur", () => {
+    if (!/^\d{3}$/.test(cvc.value)) {
+        cvcError.textContent = "CVC must be 3 digits."
+        cvc.classList.add("input-error")
+        cvc.classList.remove("input-success")
+    } else {
+        cvcError.textContent = ""
+        cvc.classList.remove("input-error")
+        cvc.classList.add("input-success")
+    }
+})
+
